a27c04381eb0dd09d04d3ad937d90dd7a38ca761
Praizl <orirepraiz98@gmail.com>
2024-10-01 16:05:41 +0100

    modified arithmetic

--- factorial/factorial.go
+++ factorial/factorial.go
@@ -1,10 +1,9 @@
 package arithmetic
 
-func IsPrime(num int) bool {
-	for i := 2; i < int(num/2); i++ {
-		if num%i == 0 {
-			return false
-		}
+func Factorial(n int) int {
+	var f int = 1
+	for i := 2; i <= n; i++ {
+		f *= i
 	}
-	return true
+	return f
 }
